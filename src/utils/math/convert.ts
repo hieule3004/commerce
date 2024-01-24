930d2135ecd9b08e28e2f16f5182a7626d887872
kai <hieu.le@dedoco.com>
2024-01-24 13:16:35 +0800

    add convert utils

--- src/utils/math/convert.ts
+++ src/utils/math/convert.ts
@@ -1,11 +1,3 @@
-import * as mathjs from 'mathjs';
+import * as convert from 'convert';
 
-
-/** Reimplementation of https://v3.yarnpkg.com/package/convert.
- * For list of units, refer to https://mathjs.org/docs/datatypes/units.html#reference.
- * */
-const convert = (value: number, fromUnit: string) => ({
-  to: (toUnit: string) => mathjs.unit(value, fromUnit).toNumber(toUnit),
-});
-
-export { convert };
+export = convert;
---
+++ src/utils/math/math.ts
@@ -0,0 +1,3 @@
+import * as mathjs from 'mathjs';
+
+export = mathjs;
