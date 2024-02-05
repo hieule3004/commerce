cc081df7c135208a4ea1adaa351926907deb1896
kai <hieu.le@dedoco.com>
2024-02-05 09:28:02 +0800

    cleanup

--- src/config/http/header.middleware.ts
+++ src/config/http/header.middleware.ts
@@ -1,15 +1,15 @@
 import { X_REQUEST_ID, X_REQUEST_TIMESTAMP } from '@src/config/http/http.constant';
 import { RequestHandler } from '@src/utils/application';
-import { nsid } from '@src/utils/nsid';
+import { decodeTime, nsid } from '@src/utils/nsid';
 
 const addCustomHeader: RequestHandler = (req, res, next) => {
-  // set custom request timestamp
-  req.headers[X_REQUEST_TIMESTAMP] = String(Date.now());
-
   // set custom request id
   const requestId = nsid();
   req.headers[X_REQUEST_ID] = requestId;
   res.setHeader(X_REQUEST_ID, requestId);
+
+  // set custom request timestamp
+  req.headers[X_REQUEST_TIMESTAMP] = String(decodeTime(requestId));
 
   next();
 };
