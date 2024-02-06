8325950ee1cd7879bc8f2fac74547602ff75f1c0
kai <hieu.le@dedoco.com>
2024-02-06 14:02:43 +0800

    cleanup

--- src/utils/application/validate.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateRequest = void 0;
-var zod_express_middleware_1 = require("zod-express-middleware");
-Object.defineProperty(exports, "validateRequest", { enumerable: true, get: function () { return zod_express_middleware_1.validateRequest; } });
