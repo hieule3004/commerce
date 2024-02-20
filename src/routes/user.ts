5a73803dd5b76ba23f8a49194f247d1d83a31a30
Hieu <hieule3004@gmail.com>
2024-02-20 22:27:39 +0800

    add repository

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,25 @@
 import { Database } from '@src/config/database/database.service';
+import { Config } from '@src/config/env/config.service';
 import { Models } from '@src/model';
 import { Application, Request, Response, asyncHandler } from '@src/utils/application';
 import { FindOptions, Model, ModelStatic } from '@src/utils/database';
 import { dasherize } from '@src/utils/string/inflection';
 
 type Context = { req: Request; res: Response };
+
+type User = Models['user'];
+
+const Repository =
+  <K extends keyof Models, Result>(
+    modelName: K,
+    injectFn: (model: ModelStatic<Model<Models[K]>>) => Result,
+  ) =>
+  (ctx: Context) => {
+    const db = ctx.req.app.get('Database') as Database;
+    const model = db.model(modelName) as ModelStatic<Model<Models[K]>>;
+    return injectFn(model);
+  };
+
 const Service =
   <Args extends unknown[], Result>(injectFn: (ctx: Context) => (...args: Args) => Result) =>
   (ctx: Context) => {
@@ -17,30 +32,27 @@ const Controller = <Result>(fn: (context: Context) => Promise<Result>) =>
     res.json({ data });
   });
 
-type User = Models['user'];
-
-const CreateUserService = Service((ctx) => {
-  const db = ctx.req.app.get('Database') as Database;
-  const userModel = db.model('user') as ModelStatic<Model<User>>;
+const CreateUserService = Repository('user', (userModel) => {
   return async (user: User) => await userModel.create(user);
 });
 
-const GetUserByIdService = Service((ctx) => {
-  const db = ctx.req.app.get('Database') as Database;
-  const userModel = db.model('user') as ModelStatic<Model<User>>;
+const GetUserByIdService = Repository('user', (userModel) => {
   return async (id: number) => userModel.findByPk(id);
 });
 
-const GetUsersService = Service((ctx) => {
-  const db = ctx.req.app.get('Database') as Database;
-  const userModel = db.model('user') as ModelStatic<Model<User>>;
+const GetUsersService = Repository('user', (userModel) => {
   return async (filter: FindOptions<User>) => await userModel.findAll(filter);
 });
 
-const AddEmailToUser = (model: Model<User>) => {
+const AddEmailToUser = (model: Model<User>, domainSupplier: () => string) => {
   const user = model.toJSON();
-  return { ...user, email: `${dasherize(user.username)}@example.com` };
+  return { ...user, email: `${dasherize(user.username)}@${domainSupplier()}` };
 };
+
+const GetEmailDomainService = Service((ctx) => {
+  const config = ctx.req.app.get('Config') as Config;
+  return () => `${config.fromEnv('npm_package_name')}.com`;
+});
 
 const CreateUserController = Controller(async (ctx) => {
   return CreateUserService(ctx)(ctx.req.body as User);
@@ -56,7 +68,8 @@ const GetUsersController = Controller(async (ctx) => {
 
 const GetUsersWithEmailController = Controller(async (ctx) => {
   const users = await GetUsersService(ctx)(ctx.req.query);
-  return users.map(AddEmailToUser);
+  const domainSupplier = GetEmailDomainService(ctx);
+  return users.map((user) => AddEmailToUser(user, domainSupplier));
 });
 
 const createRoutes = (app: Application) => {
